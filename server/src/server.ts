38d461925ab7ccff6bc2830f327246924ea4cdac
Isabelle Pinheiro <isabellepinheiro@Isabelles-MacBook-Pro.local>
2022-11-16 16:19:09 -0300

    server

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { poolRoutes } from './routes/pool'
+import { userRoutes } from './routes/user'
+import { guessRoutes } from './routes/guess'
+import { gameRoutes } from './routes/game'
+import { authRoutes } from './routes/auth'
+
+
+
+async function bootstrap(){
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true
+  })
+
+  // Em prod precisa ser env var
+  await fastify.register(jwt, {
+    secret: 'nlwcopa'
+  })
+
+
+  await fastify.register(poolRoutes)
+  await fastify.register(userRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(authRoutes)
+
+  
+  await fastify.listen({port:3333})
+}
+
+bootstrap()
